30540f99b4399117923482ed91d943ff09d2ed3f
MilkyGitHub <83046401+NekoDev-cyber@users.noreply.github.com>
2023-03-25 23:16:25 -0400

    Update Hell.js

--- Hell.js
+++ Hell.js
@@ -3,18 +3,21 @@ class random1 {
     return {
       id: '1',
       name: 'Create Window',
+      color1: '#ff0000', // pure red
+      color2: '#00ff00', // pure green
+      color3: '#0000ff',
       blocks: [
         {
           opcode: 'block1',
           blockType: Scratch.BlockType.COMMAND,
-          text: ''
+          text: 'Cock and ball torture'
         }
       ]
     };
   }
 
-  hello() {
-    return 'parent.document.body.style.backgroundColor = "lightblue"';
+  block1() {
+    return 'From the Wikipedia, The free encyclopedia.;
   }
 }
 
